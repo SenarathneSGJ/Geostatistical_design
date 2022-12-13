9ecdb164077db43749a8c470179516b80e26a9ec
Jagath Senarathne <SenarathneSGJ@users.noreply.github.com>
2022-12-13 08:43:00 +0530

    Delete Master/Example1 directory

--- Master/Example1/Rcodes/design_selection/Laplace_approx.R
+++
@@ -1,7 +0,0 @@
-Laplace_approx<- function(X,Y,theta,distM)
-  {
-   #lp.approx <- optim(par=theta,X=X, Y=Y,distM=distM, fn=log_post, hessian=TRUE,method="L-BFGS-B",lower=c(apply(prior,2,min)),upper=c(apply(prior,2,max)))
-   lp.approx <-nlminb(start=theta,X=X,distM=distM, Y=Y, objective =log_post,lower=c(apply(prior,2,min)),upper=c(apply(prior,2,max)),control = list(eval.max=10000,iter.max=10000,sing.tol=1e-18,rel.tol=1e-14)) #control = list(eval.max=10000,iter.max=10000,sing.tol=1e-18,rel.tol=1e-14),
-  
-  lp.approx
-}
